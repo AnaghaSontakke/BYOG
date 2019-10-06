0f772daee0ca05c83b6b739d585a528c72d531e4
AnaghaSontakke <48925587+AnaghaSontakke@users.noreply.github.com>
2019-10-07 02:34:18 +0530

    aoi

---
+++ Assets/MVP.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MVP : MonoBehaviour {
+
+    public Camera fpsCam;
+   // public GameObject player;
+    public GameObject prefab;
+    public float delay = 0.1f;
+    public string fullText;//can be fetched everytime from a reserve of dialogues
+    private string currentText = "";
+    // Use this for initialization
+    
+	void Update () {
+
+        Ray ray = new Ray(fpsCam.transform.position, fpsCam.transform.forward);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.Log("new ray"+ hit);
+            if (hit.collider.tag.Equals("Interact"))
+            {
+                Debug.Log("new ray colliding");
+                Instantiate(prefab, fpsCam.transform.position, Quaternion.identity);
+                StartCoroutine(ShowText());//it will call IEnumerator Show text which can use time and delays
+            }
+
+        }
+    }
+    IEnumerator ShowText()
+    {
+        for (int i = 0; i<fullText.Length; i++)
+        {
+            currentText = fullText.Substring(0, i);
+            prefab.GetComponentInChildren<TextMesh>().text = currentText;
+            yield return new WaitForSeconds(delay);
+}
+    }
+}
+
+
+   
+
