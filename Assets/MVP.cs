b1e78a44abbb1a0ad70718e52d1dd62d975f4cdf
Umesh Patil <umeshhere007@gmail.com>
2019-10-10 18:38:02 +0530

    aaj ke baad ke changes

--- Assets/MVP.cs
+++ Assets/MVP.cs
@@ -6,105 +6,76 @@ using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-public static class JsonHelper
-{
-    public static T[] FromJson<T>(string json)
-    {
-        Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(json);
-        return wrapper.Items;
-    }
+using UnityStandardAssets.Characters.FirstPerson;
 
-    public static string ToJson<T>(T[] array)
-    {
-        Wrapper<T> wrapper = new Wrapper<T>();
-        wrapper.Items = array;
-        return JsonUtility.ToJson(wrapper);
-    }
-
-    public static string ToJson<T>(T[] array, bool prettyPrint)
-    {
-        Wrapper<T> wrapper = new Wrapper<T>();
-        wrapper.Items = array;
-        return JsonUtility.ToJson(wrapper, prettyPrint);
-    }
-
-    [Serializable]
-    private class Wrapper<T>
-    {
-        public T[] Items;
-    }
-}
 
 public class MVP : MonoBehaviour {
 
-
-    [Serializable]
-    public class Interactobject {
-
-        public int phase;
-        public int milestone;
-        public string text;
-        public string[] option;
-        public string leading;
-        public string clue;
-
-    }
-    [Serializable]
-    public class RootObject
-    {
-        public Interactobject[] users;
-    }
-
     public Camera fpsCam;
-    public GameObject prefab;
-    public float delay = 0.1f;
-    public string fullText;//can be fetched everytime from a reserve of dialogues
-    private string currentText = "";
-    public List<Interactobject> items;
+    public GameObject ui;
+    public Camera temp;
+    public float delay = 0.001f;
+    public Loadjson obj;
+    public Loadjson.RootObject Alldialogs;
     public static int curPlevel = 1, curMlevel = 1;
     public static int score=0;
-    public RootObject myObject;
+    public static Quaternion fpsangle;
+    public bool selected = false;
+    public bool[] visited = new bool[50];
     // Use this for initialization
     [System.Obsolete]
 
-    void Start()
+    public void Start()
     {
-        LoadJson();
+        for (int i = 0; i < 50; i++)
+            visited[i] = false;
+        Debug.Log("started");
+        temp.enabled = false;
+        Cursor.visible = false;
+        
+        ui.GetComponent<Canvas>().enabled = false;
+        obj = new Loadjson();
+        Alldialogs = obj.Alldialogs;
+    }
+    public void act_click() {
+        score+=10;
+        Debug.Log(score);
     }
 
-    public void LoadJson()
-    {
-        using (StreamReader r = new StreamReader("Assets/json/data1.json"))
-        {
-            string json = r.ReadToEnd();
-            Debug.Log(json);
-            myObject = JsonUtility.FromJson<RootObject>("{\"users\":" + json + "}");
-            Debug.Log(myObject.users[2].leading);
-            
-            //items = JsonConvert.DeserializeObject<List<Interactobject>>(json);
-        }
+    public void button_click() {
+        selected = true;
+        ui.GetComponent<Canvas>().enabled = false;
     }
+    
+    public bool displaying = false;
 
+    [Obsolete]
     void Update () 
     {
 
         Ray ray = new Ray(fpsCam.transform.position, fpsCam.transform.forward);
         RaycastHit hit;
+       
         if (Physics.Raycast(ray, out hit))
         {
             if (hit.collider.tag.Equals("Interact"))
             {
                 Debug.Log(hit.collider.gameObject.name);
                 Debug.Log("new ray colliding");
-                if (Input.GetMouseButtonDown(0))
+                
+                if (!displaying && Input.GetMouseButtonDown(0))
                 {
+                    
+                       
                     int curhit = Convert.ToInt32(hit.collider.gameObject.name);
                     Debug.Log(curhit);
                     if (curPlevel == 1)
                     {
                         if (curhit <= 2 && curhit > 0)
                         {
-                            StartCoroutine(ShowText(myObject.users[curhit - 1].text));
+                            
+                            StartCoroutine(ShowText(curhit, Alldialogs.users[curhit - 1].text));
+                            
                         }
                     }
                     else if(curPlevel == 2)
@@ -113,35 +84,52 @@ public class MVP : MonoBehaviour {
 
 
                     }
-
-
-                   // GameObject prefabgo = Instantiate(prefab, fpsCam.transform.position, Quaternion.identity);
-                    //it will call IEnumerator Show text which can use time and delays
-                   // StartCoroutine(ShowText());
                 }
             }
 
         }
     }
-    IEnumerator ShowText(string curstring)
+    
+
+    [Obsolete]
+
+    IEnumerator ShowText(int curhit, string curstring)
     {
-        GameObject prefabgo = Instantiate(prefab, fpsCam.transform.position, Quaternion.identity);
+        displaying = true;
+        Cursor.visible = true;
+        
+        ui.GetComponent<Canvas>().enabled = true;
+        if (visited[curhit])
+        {
+            // ui.GetComponent<Button>().enabled = false;
+            GameObject.Find("act").GetComponent<Image>().enabled = false ;
+            GameObject.Find("act text").GetComponent<TextMeshPro>().enabled = false;
+        }
+        temp.transform.position = fpsCam.transform.position;
+        temp.transform.rotation = fpsCam.transform.rotation;
+        temp.enabled = true;
         fpsCam.enabled = false ;
+
         for (int i = 0; i<curstring.Length; i++)
         {
-            currentText = curstring.Substring(0, i+1);
+            string currentText = curstring.Substring(0, i+1);
             GameObject dt = GameObject.Find("dt");
             dt.GetComponent<TextMeshProUGUI>().text = currentText;
-            yield return new WaitForSeconds(delay);
+           
+;            yield return new WaitForSeconds(delay);
         }
-        Debug.Log("yay");
-        while (!Input.GetButton("ok"))
+        while (!selected)
         {
             yield return null;
         }
+        visited[curhit] = true;
+        temp.enabled = false;
+        fpsCam.transform.rotation = temp.transform.rotation;
         fpsCam.enabled = true;
-        Destroy(prefabgo);
-        
+        ui.GetComponent<Canvas>().enabled = false;
+        displaying = false;
+        selected = false;
+        Cursor.visible = false;
     }
 }
 
