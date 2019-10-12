dff31a56dfcf2cf2a73d1852f707e83d6a664486
Umesh Patil <umeshhere007@gmail.com>
2019-10-12 23:49:42 +0530

    few changes

--- Assets/Loadjson.cs
+++ Assets/Loadjson.cs
@@ -8,6 +8,7 @@ public class Loadjson : MonoBehaviour {
     [Serializable]
     public class Interactobject
     {
+        public int objref;
         public int phase;
         public int milestone;
         public string text;
@@ -15,19 +16,27 @@ public class Loadjson : MonoBehaviour {
         public string leading;
         public string clue;
     }
+   
     [Serializable]
+    
     public class RootObject
     {
         public Interactobject[] users;
     }
 
+
     public RootObject Alldialogs;
+    
     // Use this for initialization
     public Loadjson () {
         using (StreamReader r = new StreamReader("Assets/json/data1.json"))
         {
             string json = r.ReadToEnd();
+            Debug.Log(json);
             Alldialogs = JsonUtility.FromJson<RootObject>("{\"users\":" + json + "}");
         }
+
+
+        
     }
 }
--- Assets/MVP.cs
+++ Assets/MVP.cs
@@ -17,11 +17,17 @@ public class MVP : MonoBehaviour {
     public float delay = 0.001f;
     public Loadjson obj;
     public Loadjson.RootObject Alldialogs;
+    public float delayLead = 2000f;
+    public float delayClue = 3000f;
     public static int curPlevel = 1, curMlevel = 1;
     public static int score=0;
     public static Quaternion fpsangle;
     public bool selected = false;
     public bool[] visited = new bool[50];
+    public bool playeractive = false;
+    public float basetime = 0;
+    public bool click_close = false;
+    public int phase = 0, milestone = 0;
     // Use this for initialization
     [System.Obsolete]
 
@@ -29,13 +35,14 @@ public class MVP : MonoBehaviour {
     {
         for (int i = 0; i < 50; i++)
             visited[i] = false;
-        Debug.Log("started");
+
         temp.enabled = false;
         Cursor.visible = false;
-        
-        ui.GetComponent<Canvas>().enabled = false;
+    
         obj = new Loadjson();
         Alldialogs = obj.Alldialogs;
+        ui.GetComponent<Canvas>().enabled = false;
+      // StartCoroutine( Start_routine());  
     }
     public void act_click() {
         score+=10;
@@ -46,12 +53,35 @@ public class MVP : MonoBehaviour {
         selected = true;
         ui.GetComponent<Canvas>().enabled = false;
     }
-    
-    public bool displaying = false;
+    public void close_click()
+    {
+        click_close = true;
+        ui.GetComponent<Canvas>().enabled = false;
+    }
 
+    
     [Obsolete]
     void Update () 
     {
+        
+        if ((Time.time - basetime) > 10) {
+            //set lead dialogs
+            if (phase == 0)
+            {
+                if (milestone == 0)
+                {
+                    StartCoroutine(ShowTextnooptions("I should check kitchen drawer"));
+                }
+                else
+                {
+                    StartCoroutine(ShowTextnooptions(Alldialogs.users[1].leading));
+                    
+                }
+            }
+            else if (phase == 1) { }
+
+        }
+
 
         Ray ray = new Ray(fpsCam.transform.position, fpsCam.transform.forward);
         RaycastHit hit;
@@ -60,56 +90,128 @@ public class MVP : MonoBehaviour {
         {
             if (hit.collider.tag.Equals("Interact"))
             {
+
                 Debug.Log(hit.collider.gameObject.name);
-                Debug.Log("new ray colliding");
                 
-                if (!displaying && Input.GetMouseButtonDown(0))
+                if (!playeractive && Input.GetMouseButtonDown(0))
                 {
-                    
-                       
                     int curhit = Convert.ToInt32(hit.collider.gameObject.name);
-                    Debug.Log(curhit);
-                    if (curPlevel == 1)
+                    if(visited[curhit])
                     {
-                        if (curhit <= 2 && curhit > 0)
-                        {
-                            
-                            StartCoroutine(ShowText(curhit, Alldialogs.users[curhit - 1].text));
-                            
+
+                        StartCoroutine(ShowTextnooptions(Alldialogs.users[curhit].text));
+                    }
+                    if (phase == 0) 
+                         {
+
+                            //kitchen_drawer. show msg, no need for visited, set basetime to 0
+                            if (milestone == 0 && curhit == 0)
+                            {
+                                StartCoroutine(ShowTextnooptions(Alldialogs.users[curhit].text));
+                                visited[curhit] = true;
+                                milestone++;
+                            }
+                            if (milestone == 1 && curhit == 1)
+                            {
+                                StartCoroutine(ShowText(curhit));
+                                visited[curhit] = true;
+                                phase++;
+                                milestone = 0;
+                            }
+                            basetime = Time.time;
                         }
-                    }
-                    else if(curPlevel == 2)
-                    {
+                    
 
 
-
-                    }
                 }
             }
 
         }
     }
-    
+    [Obsolete]
+    IEnumerator Start_routine()
+    {
+        StartCoroutine( ShowTextnooptions("I will lead you to kitchen drawer"));
+        Debug.Log("1");
+        
+        while (true)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit,3))
+            {
+                Debug.Log("if ke andar");
+                Debug.Log(hit.collider.gameObject.name);
+                if (hit.collider.tag.Equals("Interact"))
+                {
+                    Debug.Log("abhich click kar");
+                    
+                    int curhit = Convert.ToInt32(hit.collider.gameObject.name);
+                    Debug.Log(curhit);
+                    if (curhit == 0 && (Input.GetKey("e")|| Input.GetKey("mouse 0")) )
+                    {
+                        StartCoroutine(ShowTextnooptions(Alldialogs.users[0].text)); //i am kitchen drawer
+                        break;
+                    }
+                }
+            }
+            yield return null;
+        }
+        
+        visited[0] = true;
+        basetime = Time.time;
+        while (Time.time - basetime < 10) { yield return 0; }
+        Debug.Log("4");
+        ShowTextnooptions(Alldialogs.users[1].text); //explain about game and stuff
+        Debug.Log("5");
+        playeractive = true;
+        basetime = Time.time;
 
+        while (Time.time - basetime < 10 && !visited[1]) {
+            
+        }
+        if (!visited[1]) {
+            ShowTextnooptions(Alldialogs.users[1].leading);
+        }
+        Debug.Log("6");
+        basetime = Time.time;
+
+        while (Time.time - basetime < 10 && !visited[1])
+        {
+
+        }
+        if (!visited[1])
+        {
+            ShowTextnooptions(Alldialogs.users[1].clue);
+        }
+        Debug.Log("7");
+    }
+    
     [Obsolete]
 
-    IEnumerator ShowText(int curhit, string curstring)
+    IEnumerator ShowText(int curhit)
     {
-        displaying = true;
+        playeractive = false;
         Cursor.visible = true;
-        
+       // Cursor.lockState;
+        GameObject.Find("act text").GetComponent<TextMeshProUGUI>().text = Alldialogs.users[curhit ].option[0];
+        GameObject.Find("ignore text").GetComponent<TextMeshProUGUI>().text = Alldialogs.users[curhit ].option[1];
+        GameObject.Find("option text").GetComponent<TextMeshProUGUI>().text = Alldialogs.users[curhit ].option[2];
         ui.GetComponent<Canvas>().enabled = true;
         if (visited[curhit])
         {
             // ui.GetComponent<Button>().enabled = false;
-            GameObject.Find("act").GetComponent<Image>().enabled = false ;
-            GameObject.Find("act text").GetComponent<TextMeshPro>().enabled = false;
+            GameObject.Find("choice_canvas").GetComponent<Canvas>().enabled = false;
         }
-        temp.transform.position = fpsCam.transform.position;
-        temp.transform.rotation = fpsCam.transform.rotation;
+        Vector3 pos = fpsCam.transform.position;
+        Quaternion rot = fpsCam.transform.rotation;
+        temp.transform.position = pos;
+        temp.transform.rotation = rot;
         temp.enabled = true;
         fpsCam.enabled = false ;
 
+        string curstring = Alldialogs.users[curhit].text;
+        string lead = Alldialogs.users[curhit].leading;
+        string clue = Alldialogs.users[curhit].clue;
         for (int i = 0; i<curstring.Length; i++)
         {
             string currentText = curstring.Substring(0, i+1);
@@ -118,21 +220,71 @@ public class MVP : MonoBehaviour {
            
 ;            yield return new WaitForSeconds(delay);
         }
+        
         while (!selected)
         {
             yield return null;
         }
-        visited[curhit] = true;
         temp.enabled = false;
-        fpsCam.transform.rotation = temp.transform.rotation;
+
+
         fpsCam.enabled = true;
+        fpsCam.transform.position = pos;
+        fpsCam.transform.rotation = rot;
+
+        visited[curhit] = true;
+        //fpsCam.transform.rotation = temp.transform.rotation;
+       
         ui.GetComponent<Canvas>().enabled = false;
-        displaying = false;
+        playeractive = true;
         selected = false;
         Cursor.visible = false;
     }
+
+    
+
+    [Obsolete]
+    IEnumerator ShowTextnooptions(string curstring)
+    {
+        playeractive = false;
+        Cursor.visible = true;
+        
+        ui.GetComponent<Canvas>().enabled = true;
+        GameObject.Find("choice_canvas").GetComponent<Canvas>().enabled = false;
+        
+        Vector3 pos = fpsCam.transform.position;
+        Quaternion rot = fpsCam.transform.rotation;
+        temp.transform.position = pos;
+        temp.transform.rotation = rot;
+        temp.enabled = true;
+        fpsCam.enabled = false;
+        StartCoroutine(dialog(curstring));
+        
+        while (!click_close)
+        {
+            yield return null;
+        }
+        
+        temp.enabled = false;
+        fpsCam.enabled = true;
+        fpsCam.transform.position = pos;
+        fpsCam.transform.rotation = rot;
+        ui.GetComponent<Canvas>().enabled = false;
+        click_close = false;
+        Cursor.visible = false;
+        basetime = Time.time;
+    }
+    IEnumerator dialog(string curstring)
+    {
+        for (int i = 0; i < curstring.Length; i++)
+        {
+            string currentText = curstring.Substring(0, i + 1);
+            GameObject dt = GameObject.Find("dt");
+            dt.GetComponent<TextMeshProUGUI>().text = currentText;
+
+            yield return new WaitForSeconds(delay);
+        }
+    }
 }
-
-
    
 
