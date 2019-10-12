8a1a8ca4d5b8ca1b872356f470fecca5e9ff3060
shalaka1110 <40331393+shalaka1110@users.noreply.github.com>
2019-10-13 05:10:12 +0530

    UI flow code audio

---
+++ Assets/UIbutton.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIbutton : MonoBehaviour {
+    public GameObject menuCanvas;
+    public GameObject controlsCanvas;
+    public GameObject creditsCanvas;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+    }
+
+    public void PauseButton()
+    {
+        menuCanvas.SetActive(true);
+    }
+    public void PlayButton()
+    {
+        menuCanvas.SetActive(false);
+    }
+    public void ControlsButtonMenu()
+    {
+        controlsCanvas.SetActive(true);
+    }
+    public void ControlsBackButton()
+    {
+        controlsCanvas.SetActive(false);
+    }
+    public void CreditsButtonMenu()
+    {
+        creditsCanvas.SetActive(true);
+    }
+    public void CreditsBackButton()
+    {
+        creditsCanvas.SetActive(false);
+    }
+	void Update () {
+
+    }
+}
